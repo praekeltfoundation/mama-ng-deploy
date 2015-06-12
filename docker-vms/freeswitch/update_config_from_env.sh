ad4a23cff8aed5a000afc1661507b3f4f3a32e54
Rudi Giesler <rudi@praekelt.com>
2015-06-12 16:21:24 +0200

    Use global replacements for environment variable substitutions

--- docker-vms/freeswitch/update_config_from_env.sh
+++ docker-vms/freeswitch/update_config_from_env.sh
@@ -2,12 +2,12 @@ set -e
 
 # 01_sip_dialplan.xml
 FILE="dialplan/public/01_sip_dialplan.xml"
-sed -i 's/{{number}}/'"$SIP_NUMBER"'/' "$FILE"
+sed -i 's/{{number}}/'"$SIP_NUMBER"'/g' "$FILE"
 
 # sip.xml
 FILE="sip_profiles/external/sip.xml"
-sed -i 's/{{username}}/'"$SIP_USERNAME"'/' "$FILE"
-sed -i 's/{{password}}/'"$SIP_PASSWORD"'/' "$FILE"
-sed -i 's/{{realm}}/'"$SIP_SERVER"'/' "$FILE"
-sed -i 's/{{proxy}}/'"$SIP_PROXY"'/' "$FILE"
+sed -i 's/{{username}}/'"$SIP_USERNAME"'/g' "$FILE"
+sed -i 's/{{password}}/'"$SIP_PASSWORD"'/g' "$FILE"
+sed -i 's/{{realm}}/'"$SIP_SERVER"'/g' "$FILE"
+sed -i 's/{{proxy}}/'"$SIP_PROXY"'/g' "$FILE"
 
