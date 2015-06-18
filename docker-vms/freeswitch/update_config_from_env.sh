b7bda023d1a160bfbe983dca341d6f19fc496320
Rudi Giesler <rudi@praekelt.com>
2015-06-18 13:41:51 +0200

    Change to not use WORKDIR command (thanks @hodgestar)

--- docker-vms/freeswitch/update_config_from_env.sh
+++ docker-vms/freeswitch/update_config_from_env.sh
@@ -1,11 +1,11 @@
 set -e
 
 # 01_sip_dialplan.xml
-FILE="dialplan/public/01_sip_dialplan.xml"
+FILE="/etc/freeswitch/dialplan/public/01_sip_dialplan.xml"
 sed -i 's/{{number}}/'"$SIP_NUMBER"'/g' "$FILE"
 
 # sip.xml
-FILE="sip_profiles/external/sip.xml"
+FILE="/etc/freeswitch/sip_profiles/external/sip.xml"
 sed -i 's/{{username}}/'"$SIP_USERNAME"'/g' "$FILE"
 sed -i 's/{{password}}/'"$SIP_PASSWORD"'/g' "$FILE"
 sed -i 's/{{realm}}/'"$SIP_SERVER"'/g' "$FILE"
