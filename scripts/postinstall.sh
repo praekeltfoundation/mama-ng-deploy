f24e43b244cb03fa97e695f796d1788e83360edf
Rudi Giesler <rudi@praekelt.com>
2015-06-19 13:35:43 +0200

    Add static reply app to build script (thanks @hodgestar)

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -9,3 +9,4 @@ docker build -t praekelt/mama-ng-redis $INSTALLDIR/$REPO/docker-vms/redis
 docker build -t praekelt/mama-ng-rabbitmq $INSTALLDIR/$REPO/docker-vms/rabbitmq
 docker build -t praekelt/mama-ng-freeswitch $INSTALLDIR/$REPO/docker-vms/freeswitch
 docker build -t praekelt/mama-ng-smpp $INSTALLDIR/$REPO/docker-vms/smpp_transport
+docker build -t praekelt/mama-ng-static-reply $INSTALLDIR/$REPO/docker-vms/static_reply_app
