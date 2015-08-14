2fdcb679166d3652e507880537725eb2fb19e0f1
Simon Cross <hodgestar@gmail.com>
2015-08-14 11:09:11 +0200

    Draft of Graphana container (non-working).

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -12,3 +12,4 @@ docker build -t praekelt/mama-ng-smpp $INSTALLDIR/$REPO/docker-vms/smpp_transpor
 docker build -t praekelt/mama-ng-static-reply $INSTALLDIR/$REPO/docker-vms/static_reply_app
 docker build -t praekelt/mama-ng-vumi-api $INSTALLDIR/$REPO/docker-vms/http_api
 docker build -t praekelt/mama-ng-jssandbox $INSTALLDIR/$REPO/docker-vms/jssandbox
+docker build -t praekelt/grafana $INSTALLDIR/$REPO/docker-vms/grafana
